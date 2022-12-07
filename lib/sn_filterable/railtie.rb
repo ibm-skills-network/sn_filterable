04ba306f353e9abd69feb77c2eccc28495232638
Chase McDougall <chasemcdougall@hotmail.com>
2022-12-07 16:55:22 -0500

    remove comments from railtie.rb

--- lib/sn_filterable/railtie.rb
+++ lib/sn_filterable/railtie.rb
@@ -5,11 +5,5 @@ module SnFilterable
           require_relative "main_component"
         end
       end
-    #   initializer "sn-filterable.view_helpers" do |app|
-    #     ActiveSupport.on_load(:action_view) do
-    #       include TurboScroll::ViewHelpers
-    #     end
-    #     app.config.i18n.load_path += Dir[File.expand_path(File.join(File.dirname(__FILE__), '../locales', '*.yml')).to_s]
-    #   end
     end
 end

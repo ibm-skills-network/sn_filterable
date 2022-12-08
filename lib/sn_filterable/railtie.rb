b402f5b3d4cbaff9150defee70d97eff51cae070
Chase McDougall <chasemcdougall@hotmail.com>
2022-12-08 09:35:46 -0500

    happy rubocop

--- lib/sn_filterable/railtie.rb
+++ lib/sn_filterable/railtie.rb
@@ -1,9 +1,9 @@
 module SnFilterable
-    class Railtie < ::Rails::Railtie
-      initializer "sn-filterable.load_components" do
-        ActiveSupport.on_load(:action_view) do
-          require_relative "main_component"
-        end
+  class Railtie < ::Rails::Railtie
+    initializer "sn-filterable.load_components" do
+      ActiveSupport.on_load(:action_view) do
+        require_relative "main_component"
       end
     end
+  end
 end

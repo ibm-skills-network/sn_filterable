1080ff5140c91dda3068fdef86c646374f71e0dd
Chase McDougall <chasemcdougall@hotmail.com>
2022-12-08 14:56:21 -0500

    remove commented requires

--- lib/sn_filterable.rb
+++ lib/sn_filterable.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
-# require_relative "sn_filterable/version"
-# require_relative "sn_filterable/base_components/button_component"
-# require_relative "sn_filterable/category_component"
-# require_relative "sn_filterable/chips_component"
-# require_relative "sn_filterable/filter_button_component"
-# require_relative "sn_filterable/filter_category_component"
+require_relative "sn_filterable/version"
 require_relative "sn_filterable/railtie"
-# require_relative "sn_filterable/search_component"
 require_relative "sn_filterable/filterable"
 require_relative "models/filtered"
-# require "active_support"
-# require "pg_search"
 require "kaminari"
 
 module SnFilterable

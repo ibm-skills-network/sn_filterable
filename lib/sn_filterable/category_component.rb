8af7cae1c6bdbeab3b0553f5e497683b5c8b76f8
Chase McDougall <chasemcdougall@hotmail.com>
2022-12-08 11:20:38 -0500

    Update category_component.rb

--- lib/sn_filterable/category_component.rb
+++ lib/sn_filterable/category_component.rb
@@ -1,10 +1,12 @@
+require_relative "filter_category_component"
+
 module SnFilterable
   # Renders a category to be displayed in the filtering sidebar/popup.
   # Simple view component, logic to display filters should render a [:filter] (see [Filterable::FilterCategoryComponent])
   class CategoryComponent < ViewComponent::Base
-    include HeroiconHelper
+    include Heroicon::Engine.helpers
 
-    renders_one :filter, Filterable::FilterCategoryComponent
+    renders_one :filter, SnFilterable::FilterCategoryComponent
 
     # @param [String] title Optional, the title of the category, will default to the filter's title (if specified)
     # @param [Boolean] open Optional, determines if the category should be opened by default

136218d9bc0f835bb82bda34b8494dce2c7e3a51
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2024-04-25 17:59:22 -0400

    chore: pg and pg_search are optional

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -12,10 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_12_07_175021) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-  enable_extension "unaccent"
-
   create_table "dummy_models", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

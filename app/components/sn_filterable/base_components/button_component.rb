35b34ccd7a18cd3548bc0cda5d4bccd683ea0d21
Michael Roudnitski <mroudnitski@ibm.com>
2025-01-02 15:06:13 -0500

    add dark mode support

--- app/components/sn_filterable/base_components/button_component.rb
+++ app/components/sn_filterable/base_components/button_component.rb
@@ -4,10 +4,10 @@ require_relative "base_component"
 class SnFilterable::BaseComponents::ButtonComponent < SnFilterable::BaseComponents::BaseComponent
   DEFAULT_BUTTON_TYPE = :default
   BUTTON_TYPE_MAPPINGS = {
-    DEFAULT_BUTTON_TYPE => "shadow-sm border-gray-300 text-gray-700 bg-white hover:bg-gray-50 focus:ring-violet-500",
-    :primary => "shadow-sm text-white bg-violet-600 hover:bg-violet-700 focus:ring-violet-500",
-    :danger => "border-transparent text-red-700 bg-red-100 hover:bg-red-200 focus:ring-red-500",
-    :disabled => "shadow-sm border-gray-300 text-gray-700 bg-gray-200 cursor-default"
+    DEFAULT_BUTTON_TYPE => "shadow-sm border-gray-300 dark:border-gray-600 text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 focus:ring-violet-500 dark:focus:ring-violet-400",
+    :primary => "shadow-sm text-white bg-violet-600 dark:bg-violet-700 hover:bg-violet-700 dark:hover:bg-violet-600 focus:ring-violet-500 dark:focus:ring-violet-400",
+    :danger => "border-transparent text-red-700 dark:text-red-400 bg-red-100 dark:bg-red-900 hover:bg-red-200 dark:hover:bg-red-800 focus:ring-red-500 dark:focus:ring-red-400",
+    :disabled => "shadow-sm border-gray-300 dark:border-gray-600 text-gray-700 dark:text-gray-400 bg-gray-200 dark:bg-gray-600 cursor-default"
   }.freeze
   BUTTON_TYPE_OPTIONS = BUTTON_TYPE_MAPPINGS.keys
 
@@ -56,7 +56,7 @@ class SnFilterable::BaseComponents::ButtonComponent < SnFilterable::BaseComponen
     end
 
     show_focus_ring = arguments[:show_focus_ring].nil? ? true : arguments[:show_focus_ring]
-    focus_ring_class = show_focus_ring ? "focus:ring-2 focus:ring-offset-2" : ""
+    focus_ring_class = show_focus_ring ? "focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800" : ""
 
     @arguments[:classes] = class_names(
       "inline-flex items-center border rounded-md #{focus_ring_class} focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed",
